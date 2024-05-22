94926180db5893e5b99c44eb0cc3048049a228b8
Philip Chmielowiec <philipchmielowiec@gmail.com>
2024-05-21 20:18:05 -0600

    add list_com_ports

--- ems/__init__.py
+++ ems/__init__.py
@@ -1,6 +1,7 @@
 from .core import EMS, Channel
 
 from .devices import Device
+from .utils import list_com_ports
 
 
-__all__ = ["EMS", "Device", "Channel"]
+__all__ = ["EMS", "Device", "Channel", "list_com_ports"]
---
+++ ems/utils.py
@@ -0,0 +1,8 @@
+def list_com_ports():
+    """Lists all available com ports"""
+    import serial.tools.list_ports
+
+    ports = serial.tools.list_ports.comports()
+
+    for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
