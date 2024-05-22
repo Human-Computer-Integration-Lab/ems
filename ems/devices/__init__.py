85fd88df4e6613336362d06323f59f62db587276
Philip Chmielowiec <philipchmielowiec@gmail.com>
2024-05-21 20:38:24 -0600

    update base device

--- ems/devices/__init__.py
+++ ems/devices/__init__.py
@@ -1,5 +1,11 @@
 from .rehastim import Rehastim
 from .rehamove import Rehamove
+from .base import (
+    Device,
+    IntensityConfiguration,
+    ChannelConfiguration,
+    PulseWidthConfiguration,
+)
 from .dummy import Dummy
 
 supported_devices = {"rehastim": Rehastim, "rehamove": Rehamove, "dummy": Dummy}
